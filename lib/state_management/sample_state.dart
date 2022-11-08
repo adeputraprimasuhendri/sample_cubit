302b0bbe7bacb50935bc3d441c79ddf7ca0b1cb4
adeputraprimasuhendri <ideatechnologyindonesia@gmail.com>
2022-11-08 08:20:05 +0700

    add Cubit : BlocBuilder, SampleState

--- lib/state_management/sample_state.dart
+++ lib/state_management/sample_state.dart
@@ -4,3 +4,13 @@ part of 'sample_cubit.dart';
 abstract class SampleState {}
 
 class SampleInitial extends SampleState {}
+
+class ChangeColor extends SampleState {
+  final Color color;
+  ChangeColor(this.color);
+}
+
+class ChangeText extends SampleState {
+  final String text;
+  ChangeText(this.text);
+}
